26d03b8c0712cfaad258c8efc14fcd598373b975
pavelarseyev <pasha788b@gmail.com>
2021-05-31 19:05:22 +0300

    work on both directions

--- script.js
+++ script.js
@@ -6,23 +6,32 @@ class Slider {
         this.width = this.slider.offsetWidth;
         this.trackWidth = this.track.offsetWidth;
         this.slides = this.slider.querySelectorAll('.slide');
+        this.spacing = options.spaceBetween;
         this.clones = [];
         this.paused = false;
         this.speed = this.direction === 'ltr' ? options.speed * -1 : options.speed;
-        this.offsetLeft = 0;
+        this.offsetLeft = this.direction === 'ltr' ? 0 : (this.trackWidth * -1) + ((this.slides.length) * this.spacing) - this.slides[0].offsetWidth ;
         this.frame = null;
 
-        this.track.style.willChange = 'transform';
+        this.setCss();
         this.resize();
         this.addListeners();
         this.createClones();
-        this.render();
+        setTimeout(() => {
+            console.log(this)
+            this.render(this);
+        }, 100);
     }
 
     addListeners() {
         window.addEventListener('resize', this.resize.bind(this));
         this.slider.addEventListener('mouseenter', this.changePlayState.bind(this));
         this.slider.addEventListener('mouseleave', this.changePlayState.bind(this));
+    }
+
+    setCss() {
+        this.track.style.willChange = 'transform';
+        this.slides.forEach(slide => slide.style.marginRight = `${this.spacing}px`);
     }
 
     resize() {
@@ -35,21 +44,30 @@ class Slider {
     }
 
     createClones() {
-        [...this.slides].map(slide => {
-            this.clones.push(slide);
+        Array.from(this.slides).map(slide => {
+            this.clones.push(slide.cloneNode(true));
         })
+
+        if (this.direction === 'rtl') {
+            this.clones = this.clones.reverse();
+        }
 
         this.clones.forEach(clone => {
             clone.classList.add('cloned');
-            this.track.appendChild(clone);
+            
+            if (this.direction === 'ltr') {
+                this.track.appendChild(clone);
+            } else {
+                this.track.prepend(clone);
+            }
         });
-
-        console.log(this.clones);
     }
     
     updatePosition() {
         if (!this.paused) {
             this.offsetLeft += this.speed;
+
+            // console.log(this.trackWidth);
 
             this.offsetLeft = Math.abs(this.offsetLeft) > this.trackWidth ? 0 : this.offsetLeft;
 
@@ -74,5 +92,6 @@ class Slider {
 new Slider({
     el: '.js-slider',
     speed: 1,
-    direction: 'ltr'
+    direction: 'rtl',
+    spaceBetween: 30
 });
